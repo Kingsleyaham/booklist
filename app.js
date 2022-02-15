8337574ef2db6cf9f7bf1a453f6854e2a91b2896
Aham Kingsley <kingsleyaham6@gmail.com>
2022-02-16 00:01:59 +0100

    .

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose
 
 // set up PORT AND HOST
 const port = process.env.PORT || 3000;
-const host = process.env.HOST || "https://nodejs-booklist-app.herokuapp.com";
+const host = process.env.HOST || "locahost";
 
 // set template engine to use
 app.set("view engine", "ejs");
